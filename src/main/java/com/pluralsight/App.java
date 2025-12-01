9a9bf08ed7645ae02a02abbc1395a5a365e9c695
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-01 12:58:41 -0600

    update throws ex commit

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -2,10 +2,11 @@ package com.pluralsight;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila";
         String username = "root";
         String password = "yearup";
